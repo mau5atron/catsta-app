a61a9271d1a24b66d2285eb7c4a5c721cbe06480
mau5atron <gabrielbetancourt96@outlook.com>
2017-11-03 14:21:24 -0400

    updated controller and view for cat api call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	@cats = Cat.get_cats
   end
 
-  def self.get_cats
-		url = @@url_base + "/images/get?format=xml&results_per_page=40"
-		return HTTParty.get(url).parsed_response["response"]["data"]["images"]["image"]
-	end
+
 end
