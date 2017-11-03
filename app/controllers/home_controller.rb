af7bb352b0013da95e9c71144f7fad7c62d99f58
mau5atron <gabrielbetancourt96@outlook.com>
2017-11-03 14:12:13 -0400

    added api call to Cat, require httparty gem, bundle installed// next step: render view ....

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def self.get_cats
+		url = @@url_base + "/images/get?format=xml&results_per_page=40"
+		return HTTParty.get(url).parsed_response["response"]["data"]["images"]["image"]
+	end
 end
